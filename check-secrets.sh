efdb8a4fd8440c49212b27eb83d8ea30adce59ee
Dao Lam <daolam112@gmail.com>
2016-09-28 12:05:36 -0700

    Create check-secrets.sh

---
+++ check-secrets.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+# FOO = Fake secret for functional tests
+if [ -z "$FOO" ]]; then
+  echo Failing the step, missing environment variables FOO
+  exit 1
+fi
+
+echo Get FOO secret
+exit 0
